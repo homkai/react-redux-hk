c88e0eb379e7071785d30057d92e6797628c84eb
Homkai <homkai@qq.com>
2016-12-13 02:19:25 +0800

    update

---
+++ README.md
@@ -0,0 +1,45 @@
+# react-redux-hk
+自动分析connect依赖的state，当且仅当mapStateToProps依赖的state改变时，才会重新计算mapStateToProps，进而触发重新渲染Component
+
+完全兼容react-redux的api，可直接替换
+
+# 解决什么问题
+如下代码：
+```js
+const state = {
+    listData: [],
+    planId: 0,
+    unitId: 0,
+    showEditor: false
+};
+
+function getCrumbInfo(state) {
+    const plan = state.listData.filter(item => item.id === state.planId)[0];
+    const unit = plan.unitList.filter(item => item.id === state.unitId)[0];
+    return {
+        planName: plan.planName,
+        unitName: unit.unitName
+    };
+}
+
+function mapStateToProps(state) {
+    return {
+        planList: state.listData,
+        crumbInfo: getCrumbInfo(state)
+    };
+}
+```
+mapStateToProps返回的crumbInfo，即使实际依赖的listData、planId、unitId没有变，
+getCrumbInfo每次执行return的引用都是不同的，这会导致react-redux的shallowEqual不一致，
+进而会重新渲染connect的Component
+
+业务代码中，这样的例子很常见，导致某一局部状态更新，牵一发而动全身，很多地方不需要重新渲染的，也重新渲染，大大降低页面性能
+
+# 常见的方案
+- Immutable.js 引入这个库的成本很高，需要很多地方都改成Immutable.js的API
+- reselect 使用这个库也比较麻烦，因为需要显式地用function声明很多状态的依赖，导致mapStateToProps很复杂
+
+# react-redux-hk的思路
+自动分析mapStateToProps依赖的state，只有当依赖的state改变时，才会重新计算mapStateToProps，进而触发重新渲染Component
+
+**不需要额外处理，即可自动完成性能优化**
---
+++ src/index.js
@@ -0,0 +1,103 @@
+/**
+ * Created by Homkai on 16/12/12.
+ */
+import React from 'react';
+import {connect as rrConnect, Provider} from 'react-redux';
+import omit from 'lodash/omit';
+
+const __depState__ = {};
+const __trueState__ = [];
+
+function connect(mapStateToProps, mapDispatchToProps, mergeProps = undefined, options = {}) {
+    // 依赖的state是否pure——每次mapStateToProps执行依赖的state是否一样
+    const {pureDepState = true} = options;
+    return Component => {
+        const mapDepState = !mapStateToProps ? undefined : (store, ownProps) => {
+            const uid = mapStateToProps.toString();
+            const {depState, storeProxy} = getDepStateAndStoreProxy(uid, store, pureDepState);
+            __trueState__[0] = storeProxy
+                // 初次connect时，执行
+                ? mapStateToProps(storeProxy, ownProps)
+                : mapStateToProps.bind(null, store, ownProps);
+            const props = depState;
+            props.__trueState__ = __trueState__;
+            const tempList = [...Object.keys(props), '__tempList__'];
+            return {
+                ...props,
+                __tempList__: tempList.join(',')
+            };
+        };
+        Component = getGuardComponent(Component);
+        return rrConnect(
+            mapDepState,
+            mapDispatchToProps,
+            mergeProps,
+            options
+        )(Component);
+    };
+}
+
+function getGuardComponent(UI) {
+    return props => {
+        let trueState = props.__trueState__[0];
+        // 把mapStateToProps的计算逻辑放到WrappedUIComponent渲染时执行，减少不必要的性能损耗
+        trueState = typeof trueState === 'function' ? trueState() : trueState;
+        const stateAndCallbacks = {
+            ...omit(props, props.__tempList__.split(',')),
+            ...trueState
+        };
+        return <UI {...stateAndCallbacks} />
+    };
+}
+
+function getDepStateAndStoreProxy(uid, store, pureDepState) {
+    // 如果是pureDepState，则只分析一次依赖的state，再次执行直接返回新的depState
+    if (pureDepState && __depState__[uid]) {
+        const depState = __depState__[uid];
+        Object.keys(depState).forEach(path => (depState[path] = getStateByPath(store, path)));
+        return {depState};
+    }
+    __depState__[uid] = __depState__[uid] || {};
+    const depState = __depState__[uid];
+    const storeProxy = {};
+    Object.keys(store).forEach(namespace => {
+        Object.defineProperty(storeProxy, namespace, {
+            get() {
+                const obj = {...store[namespace]};
+                Object.keys(store[namespace]).forEach(key => {
+                    Object.defineProperty(obj, key, {
+                        get() {
+                            const value = store[namespace][key];
+                            depState[namespace + '/' + key] = value;
+                            // 判断是否依赖整个model的state
+                            if (depState[namespace]) {
+                                const keyList = Object.keys(depState).filter(key => !key.indexOf(namespace + '/'));
+                                if (keyList.length === Object.keys(depState[namespace]).length) {
+                                    delete depState[namespace];
+                                }
+                            }
+                            return value;
+                        }
+                    });
+                });
+                depState[namespace] = store[namespace];
+                return obj;
+            }
+        });
+    });
+    return {
+        depState,
+        storeProxy
+    };
+}
+
+function getStateByPath(state, path) {
+    const [namespace, field] = path.split('/');
+    return field ? state[namespace][field] : state[namespace];
+}
+
+
+export {
+    Provider,
+    connect
+};
