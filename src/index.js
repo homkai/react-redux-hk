7c2c2b9d2ca2f1db7adbee97142d5ed8565d2f1e
Homkai <wanghongkai@baidu.com>
2017-09-26 21:02:46 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,22 +7,24 @@ import shallowEqual from 'react-redux/lib/utils/shallowEqual';
 
 const PATH_SEP = '//';
 const DEP_STATE_DEPTH = 2;
-const DEP_OWN_PROPS_DEPTH = 1;
 
 const __depCache__ = {};
 const __trueState__ = {};
 
-function connect(mapStateToProps, mapDispatchToProps, mergeProps = undefined, options = {}) {
+function connect(mapStateToProps, mapDispatchToProps, mergeProps, options) {
     // pureMapState表示mapStateToProps是否是纯函数，只依赖state和ownProps
-    let {pureMapState = true, depStateDepth = DEP_STATE_DEPTH} = options;
+    let {pureMapState = true, depStateDepth = DEP_STATE_DEPTH} = options || {};
     const uid = mapStateToProps.toString() + PATH_SEP + Math.random();
     return Component => {
         const mapDepState = !mapStateToProps
             ? undefined
             : (state, ownProps) => {
+                if (!pureMapState) {
+                    return mapStateToProps(state, ownProps);
+                }
                 let depState = null;
                 // 如果是pureMapState则优先读缓存，避免重复分析depState
-                if (pureMapState && getDepStateCache(uid)) {
+                if (getDepStateCache(uid)) {
                     depState = {};
                     Object.keys(getDepStateCache(uid)).forEach(path => (depState[path] = getValueByPath(state, path)));
                     // 如果新的depState有更新（shallowEqual不一致），则重新分析依赖
@@ -73,33 +75,33 @@ function buildGuardComponent(Component) {
 
 // 分析mapStateToProps真正依赖到的状态 depState
 function analyzeDepState(uid, state, ownProps, mapState, depStateDepth) {
-    const stateUid = uid + PATH_SEP + 'state';
-    const ownPropsUid = uid + PATH_SEP + 'ownProps';
-    const {cacheDep: cacheDepState, dep: depState} = buildCacheDep(stateUid);
-    const stateProxy = proxyObj(state, cacheDepState, depStateDepth);
-    const {cacheDep: cacheDepProps, dep: depProps} = buildCacheDep(ownPropsUid);
-    const propsProxy = proxyObj(ownProps, cacheDepProps, DEP_OWN_PROPS_DEPTH);
-    const stateProps = mapState(stateProxy, propsProxy);
-
-    const ret = {stateProps};
-
+    const ret = {};
     // 判断是否依赖ownProps，如果依赖，则不缓存
-    ret.cacheable = !Object.keys(depProps).length;
-    removeDepCache(ownPropsUid);
-
-    // 过滤依赖obj.key而不依赖obj的depState
-    const depObjStatePathList = Object.keys(depState)
-        .filter(item => item.split(PATH_SEP).length <= depStateDepth && typeof depState[item] === 'object');
-    if (depObjStatePathList.length) {
-        const depObjStateRefList = depObjStatePathList.map(item => depState[item]);
-        removeObjStateDep(depObjStateRefList, stateProps);
-        Object.keys(depState).forEach(key => {
-            if (depObjStateRefList.indexOf(depState[key]) > -1) {
-                delete depState[key];
-            }
-        });
+    ret.cacheable = !ownProps || !Object.keys(ownProps).length;
+    if (ret.cacheable) {
+        const stateUid = uid + PATH_SEP + 'state';
+        const {cacheDep: cacheDepState, dep: depState} = buildCacheDep(stateUid);
+        const stateProxy = proxyObj(state, cacheDepState, depStateDepth);
+        const stateProps = mapState(stateProxy);
+        // 过滤依赖obj.key而不依赖obj的depState
+        const depObjStatePathList = Object.keys(depState)
+            .filter(item => item.split(PATH_SEP).length <= depStateDepth && typeof depState[item] === 'object');
+        if (depObjStatePathList.length) {
+            const depObjStateRefList = depObjStatePathList.map(item => depState[item]);
+            removeObjStateDep(depObjStateRefList, stateProps);
+            Object.keys(depState).forEach(key => {
+                if (depObjStateRefList.indexOf(depState[key]) > -1) {
+                    delete depState[key];
+                }
+            });
+        }
+        ret.depState = depState;
+        ret.stateProps = stateProps;
     }
-    ret.depState = depState;
+    else {
+        ret.depState = {};
+        ret.stateProps = mapState(state, ownProps);
+    }
 
     return ret;
 }
@@ -140,10 +142,6 @@ function updateDepStateCache(uid, depState) {
     }
     __depCache__[uid + PATH_SEP + 'state'] = {...depState};
     return true;
-}
-
-function removeDepCache(uid) {
-    delete __depCache__[uid];
 }
 
 function proxyObj(obj, cb, depth = 100, keyPath = PATH_SEP) {
